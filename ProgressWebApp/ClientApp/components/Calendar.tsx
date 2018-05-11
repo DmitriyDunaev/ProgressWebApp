af8892397239e0e37430857b3ad49eadf65fb48b
daniil.bsd <daniil.bsd@gmail.com>
2018-05-11 23:16:20 +0200

    Read some stuff on the subject. As the result: 1) Created a proper way of storring dynamic attributes (State) 2) Created a simple addEvnt method that is a very abstract. 3) Created a simple removeEvent(s) method that is also very basic, but does the job in proper way.
    Please continue your research from this point on.
    Tomorrow I will focus on creating an event from selected range.

--- ProgressWebApp/ClientApp/components/Calendar.tsx
+++ ProgressWebApp/ClientApp/components/Calendar.tsx
@@ -5,39 +5,82 @@ import * as moment from 'moment';
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
 
-const dummyEvents = [
-        {
-            allDay: false,
-            end: new Date('2018-04-23 11:13:00'),
-            start: new Date('2018-04-22 10:13:00'),
-            title: 'hi',
-        },
-        {
-            allDay: true,
-            end: new Date('2018-04-25 11:13:00'),
-            start: new Date('2018-04-25 11:13:00'),
-            title: 'All Day Event',
-        },
-    ];
+//Initial events, Fetcher should be written in the constructor
+const eventsFromSomewhere = [
+    {
+        allDay: false,
+        end: new Date('2018-05-23 11:13:00'),
+        start: new Date('2018-05-23 10:13:00'),
+        title: 'hi',
+    },
+    {
+        allDay: true,
+        end: new Date('2018-05-25 11:13:00'),
+        start: new Date('2018-05-25 11:13:00'),
+        title: 'All Day Event',
+    },
+];
+
+//event template
+const additionalEvent = {
+    allDay: false,
+    end: new Date('2018-05-24 11:13:30'),
+    start: new Date('2018-05-24 11:13:00'),
+    title: 'Added event',
+};
 
 
-export class Calendar extends React.Component<RouteComponentProps<{}>, {}> {
+//this is what is how we declare state varibles (dynamic attributes)
+interface CalendarComponentState {
+    events: {
+        allDay: boolean,
+        end: Date,
+        start: Date,
+        title: string,
+    }[]
+}
+
+export class Calendar extends React.Component<RouteComponentProps<{}>, CalendarComponentState> {//interface is used here
+   
+
+    //Simple example of adding an event 
+    addEvent() {
+        this.state.events.push(additionalEvent);
+        this.forceUpdate();
+    }
+
+    //simple example of removing an element by name
+    removeEvents() {
+        this.setState({
+            events: this.state.events.filter(function (e) {
+                return e.title !== 'Added event'
+            })
+        });
+    }
+
     constructor() {
         super();
+        this.state = {
+            events: eventsFromSomewhere // Fetch function goes here
+        }
         BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
     }
-    
- 
+
+
     public render() {
-        return <div style={{ height: '100vh', margin: '10px' }}>
-        <BigCalendar
-                events={dummyEvents}
-                startAccessor="start"
-                endAccessor="end"
-                min-height="800px"
-                defaultDate={new Date()}
-            />
+        return <div>
+            <button onClick={() => { this.addEvent() }}>add event</button>
+            <button onClick={() => { this.removeEvents() }}>remove all added events</button>
+            <div style={{ height: '100vh', margin: '10px' }}>
+                <BigCalendar
+                    events={ this.state.events }
+                    startAccessor="start"
+                    endAccessor="end"
+                    min-height="800px"
+                    defaultDate={new Date()}
+                />
             </div>
-        
+        </div>
+
     }
 }
