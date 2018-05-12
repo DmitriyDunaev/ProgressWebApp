051715710ae3b577966e815c5d0ab2aabf78e732
daniil.bsd <daniil.bsd@gmail.com>
2018-05-12 12:20:43 +0200

    "Drawing" new events is now possible. Props like behavior is implemented. For details read the comments.

--- ProgressWebApp/ClientApp/components/Calendar.tsx
+++ ProgressWebApp/ClientApp/components/Calendar.tsx
@@ -4,8 +4,8 @@ import BigCalendar from 'react-big-calendar';
 import * as moment from 'moment';
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
-
-//Initial events, Fetcher should be written in the constructor
+// TEMPERAL
+//Initial events, Fetcher should be written in the constructor  (will be removed)
 const eventsFromSomewhere = [
     {
         allDay: false,
@@ -21,17 +21,32 @@ const eventsFromSomewhere = [
     },
 ];
 
-//event template
+//event instance (will be removed)
 const additionalEvent = {
     allDay: false,
     end: new Date('2018-05-24 11:13:30'),
     start: new Date('2018-05-24 11:13:00'),
     title: 'Added event',
 };
+// TYPES
 
+interface Event {
+    allDay: boolean,
+    start:  Date,
+    end:  Date,
+    title: string,
+}
+
+
+interface SelectionRange {
+    start:  Date,
+    end: Date,
+    slots: Date[] | string[],
+    action: "select" | "click" | "doubleClick"
+}
 
 //this is what is how we declare state varibles (dynamic attributes)
-interface CalendarComponentState {
+interface CalendarState {
     events: {
         allDay: boolean,
         end: Date,
@@ -40,17 +55,32 @@ interface CalendarComponentState {
     }[]
 }
 
-export class Calendar extends React.Component<RouteComponentProps<{}>, CalendarComponentState> {//interface is used here
-   
+//***** ***** ***** USING THIS IS RIGHT BUT REQUIRES CHANGES IN HOME.TSX ***** ***** *****
 
+//this is what is how we declare prop varibles (readoly attributes)
+//interface CalendarProps extends RouteComponentProps<{}> {
+//    step: number, // length of a time step (mins)
+//    timeslot: number, // interval between displayed values mesured in steps
+//    minEventDuration: number, //mesured in steps 
+//    maxEventDuration: number, //mesured in steps 
+//}
+
+//***** ***** ***** *****  *****  *****  КАСТЫЛЬ :P *****  *****  ***** ***** ***** *****
+const properties = { step: 15, timeslot: 2, minEventDuration: 4, maxEventDuration: 9 };
+//***** ***** ***** *****  *****  ***** ***** ***** *****  *****  ***** ***** ***** *****
+
+export class Calendar extends React.Component<RouteComponentProps<{}>, CalendarState> {//interface is used here
+
+    //**** Proof of Concept ****
     //Simple example of adding an event 
-    addEvent() {
-        this.state.events.push(additionalEvent);
+    AddEvent(event: Event) {
+        this.state.events.push(event);
         this.forceUpdate();
     }
 
+    //**** Proof of Concept ****
     //simple example of removing an element by name
-    removeEvents() {
+    RemoveEvents() {
         this.setState({
             events: this.state.events.filter(function (e) {
                 return e.title !== 'Added event'
@@ -58,6 +88,39 @@ export class Calendar extends React.Component<RouteComponentProps<{}>, CalendarC
         });
     }
 
+    //Branches emptiness selection handeling by action
+    SelectionHandler(slotInfo: SelectionRange) {
+        switch (slotInfo.action) {
+            case "select":
+                this.SpecificRangeSelection(slotInfo);
+                break;
+            case "click":
+                this.StartSelection();
+                break;
+            case "select":
+                this.DoubleClickEmptinessHandler();
+        }
+    }
+
+    //TODO:
+    // 1) Dialog for creating multiple small events in place of oversized.
+    // 2) Creating a dialog for the name and, in the future other properties.
+    // 3) Separate handeling for multi-day selection (if needed).
+    SpecificRangeSelection(selection: SelectionRange) {
+        if (selection.start.getDate() == selection.end.getDate() &&
+            selection.start.getMonth() == selection.end.getMonth() &&
+            selection.start.getFullYear() == selection.end.getFullYear() &&
+            selection.slots.length >= properties.minEventDuration &&
+            selection.slots.length <= properties.maxEventDuration) {
+            this.AddEvent({ allDay: false, start: selection.start, end: selection.end, title: "Drawn Event" });
+        }
+    }
+
+    //TODO
+    StartSelection() { }
+    DoubleClickEmptinessHandler() { }
+
+   
     constructor() {
         super();
         this.state = {
@@ -69,13 +132,24 @@ export class Calendar extends React.Component<RouteComponentProps<{}>, CalendarC
 
     public render() {
         return <div>
-            <button onClick={() => { this.addEvent() }}>add event</button>
-            <button onClick={() => { this.removeEvents() }}>remove all added events</button>
+            <button onClick={() => { this.AddEvent(additionalEvent) }}>add event</button>
+            <button onClick={() => { this.RemoveEvents() }}>remove all added events</button>
             <div style={{ height: '100vh', margin: '10px' }}>
                 <BigCalendar
-                    events={ this.state.events }
+                    events={this.state.events}
                     startAccessor="start"
                     endAccessor="end"
+                    step={properties.step}      // All events and selections are rounded to [15] minutes.
+                    timeslots={properties.timeslot}  // determines how often the time values are displayed in week and day views (in steps) (15 * 2 = 30 minutes)
+                    selectable     // For the line below
+                    onSelectSlot={(e) => {
+                        this.SelectionHandler({
+                            start: new Date(e.start.toString()),  //Needs to be fixed!!!!
+                            end: new Date(e.end.toString()),      //Needs to be fixed!!!!
+                            slots: e.slots,
+                            action: e.action
+                        })
+                    }} // Meant to be used for "drawing" events.
                     min-height="800px"
                     defaultDate={new Date()}
                 />
@@ -83,4 +157,5 @@ export class Calendar extends React.Component<RouteComponentProps<{}>, CalendarC
         </div>
 
     }
+
 }
